3853368ead9aab5c66b8b92c7e8ad6fdb517f2c8
mhill44 <3452@holbertonschool.com>
2022-09-01 21:16:24 -0500

    fixing PEP8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,7 +6,7 @@ A Method that determines if all of the boxes can be opened or not
 
 
 def canUnlockAll(boxes):
-    
+
     if (len(boxes) == 0):
         return False
     box = list(range(len(boxes)))

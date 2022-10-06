f8250b81492944d58fd38a1f14a3e3b06b2e9de5
mhill44 <3452@holbertonschool.com>
2022-10-06 03:22:43 -0500

    fixing syntax

---
+++ 0x03-sandpiles/sandpiles.h
@@ -0,0 +1,15 @@
+#ifndef _SANDPILES_H
+#define _SANDPILES_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void p(int grid[3][3]);
+
+#endif /* _SANDPILES_H */
+
